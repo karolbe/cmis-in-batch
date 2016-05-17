a9b0b1e78e1b035c1263776787b3bd78bd011263
Karol Bryd <karolbe@gmail.com>
2016-05-17 22:04:06 +0200

    Preparation for the 1.0 release.

--- src/test/java/com/metasys/cmis/CMISInBatchTest.java
+++ src/test/java/com/metasys/cmis/CMISInBatchTest.java
@@ -1,6 +1,5 @@
 package com.metasys.cmis;
 
-import com.metasys.CMISInBatch;
 import org.junit.Test;
 
 /**
@@ -10,14 +9,6 @@ public class CMISInBatchTest {
 
     @Test
     public void testSomething() throws Exception {
-        System.out.println("test something");
-
-        CMISInBatch cmisInBatch = new CMISInBatch();
-
-        cmisInBatch.init("target/classes/com/metasys/alfresco/import_data_example.sdl");
-
-
+        System.out.println("Maybe later...");
     }
-
-
 }
